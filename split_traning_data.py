508a5e6835da43b7db7ab646bdf4836979c122e7
Kacper Marciniak <91064556+Kacper-Marciniak@users.noreply.github.com>
2022-06-14 15:40:45 +0200

    Small changes to the code

--- split_traning_data.py
+++ split_traning_data.py
@@ -4,10 +4,12 @@ from shutil import copyfile
 
 from PARAMETERS import PATH_TRAINING_DATA_SEGMENTATION
 
+#TODO: Improve splitting process
+
 DATA_PATH = PATH_TRAINING_DATA_SEGMENTATION
 
 val_precentage = 0.15 #15% 
-test_precentage = 0.3 #15% (15-30)
+test_precentage = val_precentage+0.15 #15% (15-30)
 
 
 def split_data():
@@ -17,37 +19,37 @@ def split_data():
         split_name = file.split('.')[0]
         r = random()
         if(r < val_precentage): #VAL
-            copyfile(DATA_PATH + r'/anot'+ '/'+ split_name + '.json',DATA_PATH + r'/val'+ '/'+ split_name + '.json')
-            copyfile(DATA_PATH + r'/anot'+ '/'+ split_name + '.png',DATA_PATH + r'/val'+ '/'+ split_name + '.png')
-            os.remove(DATA_PATH + r'/anot'+ '/'+ split_name + '.png')
-            os.remove(DATA_PATH + r'/anot'+ '/'+ split_name + '.json')
+            copyfile(DATA_PATH + r'/anot/'+ split_name + '.json',DATA_PATH + r'/val/'+ split_name + '.json')
+            copyfile(DATA_PATH + r'/anot/'+ split_name + '.png',DATA_PATH + r'/val/'+ split_name + '.png')
+            os.remove(DATA_PATH + r'/anot/'+ split_name + '.png')
+            os.remove(DATA_PATH + r'/anot/'+ split_name + '.json')
         if(val_precentage < r < test_precentage): #TEST
-            copyfile(DATA_PATH + r'/anot'+ '/'+ split_name + '.json',DATA_PATH + r'/test'+ '/'+ split_name + '.json')
-            copyfile(DATA_PATH + r'/anot'+ '/'+ split_name + '.png',DATA_PATH + r'/test'+ '/'+ split_name + '.png')
-            os.remove(DATA_PATH + r'/anot'+ '/'+ split_name + '.png')
-            os.remove(DATA_PATH + r'/anot'+ '/'+ split_name + '.json')
+            copyfile(DATA_PATH + r'/anot/'+ split_name + '.json',DATA_PATH + r'/test/'+ split_name + '.json')
+            copyfile(DATA_PATH + r'/anot/'+ split_name + '.png',DATA_PATH + r'/test/'+ split_name + '.png')
+            os.remove(DATA_PATH + r'/anot/'+ split_name + '.png')
+            os.remove(DATA_PATH + r'/anot/'+ split_name + '.json')
         if(test_precentage < r): #TRAIN
-            copyfile(DATA_PATH + r'/anot'+ '/'+ split_name + '.json',DATA_PATH + r'/train'+ '/'+ split_name + '.json')
-            copyfile(DATA_PATH + r'/anot'+ '/'+ split_name + '.png',DATA_PATH + r'/train'+ '/'+ split_name + '.png')
-            os.remove(DATA_PATH + r'/anot'+ '/'+ split_name + '.png')
-            os.remove(DATA_PATH + r'/anot'+ '/'+ split_name + '.json')
+            copyfile(DATA_PATH + r'/anot/'+ split_name + '.json',DATA_PATH + r'/train/'+ split_name + '.json')
+            copyfile(DATA_PATH + r'/anot/'+ split_name + '.png',DATA_PATH + r'/train/'+ split_name + '.png')
+            os.remove(DATA_PATH + r'/anot/'+ split_name + '.png')
+            os.remove(DATA_PATH + r'/anot/'+ split_name + '.json')
 
 def merge_data():
     # Before splitting put all data with annotations to the traing folder
     files = os.listdir(DATA_PATH + r'/val')
     for file in files:
-        copyfile(DATA_PATH + r'/val'+ '/'+ file, DATA_PATH + r'/anot'+ '/'+ file)
-        os.remove(DATA_PATH + r'/val'+ '/'+ file)
+        copyfile(DATA_PATH + r'/val/'+ file, DATA_PATH + r'/anot/'+ file)
+        os.remove(DATA_PATH + r'/val/'+ file)
         
     files = os.listdir(DATA_PATH + r'/test')
     for file in files:
-        copyfile(DATA_PATH + r'/test'+ '/'+ file, DATA_PATH + r'/anot'+ '/'+ file)
-        os.remove(DATA_PATH + r'/test'+ '/'+ file)
+        copyfile(DATA_PATH + r'/test/'+ file, DATA_PATH + r'/anot/'+ file)
+        os.remove(DATA_PATH + r'/test/'+ file)
     
     files = os.listdir(DATA_PATH + r'/train')
     for file in files:
-        copyfile(DATA_PATH + r'/train'+ '/'+ file, DATA_PATH + r'/anot'+ '/'+ file)
-        os.remove(DATA_PATH + r'/train'+ '/'+ file)
+        copyfile(DATA_PATH + r'/train/'+ file, DATA_PATH + r'/anot/'+ file)
+        os.remove(DATA_PATH + r'/train/'+ file)
         
 
 
