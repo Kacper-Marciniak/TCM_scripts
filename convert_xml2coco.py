4e9a361110302aed0a28515c7631cfa82f5b269d
Kacper Marciniak <91064556+Kacper-Marciniak@users.noreply.github.com>
2022-06-14 15:43:00 +0200

    removed unnecessary comments

--- convert_xml2coco.py
+++ convert_xml2coco.py
@@ -1,8 +1,3 @@
-  
-#!/usr/bin/python
-
-# pip install lxml
-
 import sys
 import os
 import json
@@ -11,13 +6,6 @@ import glob
 
 START_BOUNDING_BOX_ID = 1
 PRE_DEFINE_CATEGORIES = None
-# If necessary, pre-define category and its id
-#  PRE_DEFINE_CATEGORIES = {"aeroplane": 1, "bicycle": 2, "bird": 3, "boat": 4,
-#  "bottle":5, "bus": 6, "car": 7, "cat": 8, "chair": 9,
-#  "cow": 10, "diningtable": 11, "dog": 12, "horse": 13,
-#  "motorbike": 14, "person": 15, "pottedplant": 16,
-#  "sheep": 17, "sofa": 18, "train": 19, "tvmonitor": 20}
-
 
 def get(root, name):
     vars = root.findall(name)
