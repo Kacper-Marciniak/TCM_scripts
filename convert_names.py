59b294ecb6f07e8a10afed8e93aef42b3d1012b3
Kacper-Marciniak <kacpermarciniak1998@gmail.com>
2022-06-15 12:32:51 +0200

    Minor fixes

--- convert_names.py
+++ convert_names.py
@@ -2,14 +2,11 @@ import pandas as pd
 import numpy as np
 import os
 
-#TODO: improve name changing process
-
 FILE = 'test.csv'
 PATH = r'D:\Konrad\TCM_scan\dash_skany\test'
-
 DASH = r'D:\Konrad\TCM_scan\dash'
 
-df = pd.read_csv(DASH + '\\' + FILE) 
+df = pd.read_csv(os.path.join(DASH.FILE)) 
 l_elements = df['l_id'].unique()
 w_elements = df['w_id'].unique()
 w_elements[::-1].sort()
@@ -18,17 +15,17 @@ l_elements[::-1].sort()
 print(l_elements)
 print(w_elements)
 
-files = list(os.listdir(PATH + r'/images'))
+files = list(os.listdir(os.path.join(PATH,'images')))
 for image_name in files: # Iterate over files
     
-    base_name = image_name[:image_name.rfind('.')]
+    base_name = image_name.split('.')[-2]
     split_name = base_name.split('_')
-    row = int(split_name[1])
-    col = int(split_name[0])
+    row = int(split_name[-1])
+    col = int(split_name[-2])
     print(np.where(l_elements == col))
     id_l = np.where(l_elements == col)[0][0]
     id_w = np.where(w_elements == row)[0][0]
     print(image_name, id_l, id_w)
-    new_name = str(id_l+1) + '_' + str(id_w+1) + '.png'
-    print(image_name,'------>',new_name)
-    os.rename(PATH + r'/images/' + image_name, PATH + r'/images/' + new_name)
+    new_name = f"{str(id_l+1)}_{str(id_w+1)}.png"
+    print(f"{image_name} -> {new_name}")
+    os.rename(f'{PATH}/images/{image_name}', f'{PATH}/images/{new_name}')
