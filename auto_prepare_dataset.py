d05b283ff7df642c3cccd092b42d4c6cfcc8892a
Kacper-Marciniak <kacpermarciniak1998@gmail.com>
2022-06-15 15:13:46 +0200

    Small script for automating dataset preparation

---
+++ auto_prepare_dataset.py
@@ -0,0 +1,4 @@
+#import copyfiles # Run copying script
+import split_training_data # Run splitting script
+import convert_labelme2coco # Run conversion script
+import count_classes_dataset # Run counting script
