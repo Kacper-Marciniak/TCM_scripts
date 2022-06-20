b9acee938da62df73bf20eecefd5dd2c312693f2
Kacper-Marciniak <kacpermarciniak1998@gmail.com>
2022-06-20 14:23:17 +0200

    Info file generation, custom and random seeds for shuffling function

--- split_training_data.py
+++ split_training_data.py
@@ -2,16 +2,20 @@ import glob, os
 import random
 from shutil import copyfile
 
+import time
+import datetime
+import json
+
 from PARAMETERS import PATH_TRAINING_DATA_SEGMENTATION
 from tkinter_dialog_custom import askdirectory
 
-#TODO: Improve splitting process
 
-DATASET_PATH = askdirectory(title="Select dataset directory", initialdir=PATH_TRAINING_DATA_SEGMENTATION)
+DATASET_PATH = askdirectory(title="Select dataset directory", initialdir=PATH_TRAINING_DATA_SEGMENTATION).replace("\\\\","\\").replace("\\","/")
 
+#Parameters
 val_precentage = 0.15
 test_precentage = 0.15
-
+custom_seed = None
 
 def split_data():
     # Split data into train val and test
@@ -29,6 +33,11 @@ def split_data():
         "Val": round(val_precentage*len_list_files),
     }
 
+    if custom_seed == None: 
+        shuffle_seed = int(time.time()) # get unique seed
+    else:
+        shuffle_seed = custom_seed # use custom seed
+    random.seed(shuffle_seed)
     random.shuffle(list_files)
 
     list_files_test = list_files[0:subset_lens["Test"]]
@@ -55,6 +64,8 @@ def split_data():
         copyfile(f'{DATASET_PATH}/anot/{split_name}.json',f'{DATASET_PATH}/train/{split_name}.json')
         copyfile(f'{DATASET_PATH}/anot/{split_name}.png',f'{DATASET_PATH}/train/{split_name}.png')
         os.remove(f'{DATASET_PATH}/anot/{split_name}.png')
+    
+    return shuffle_seed, len_list_files
 
 def merge_data():
     # Before splitting put all data with annotations to the traing folder
@@ -80,8 +91,24 @@ def merge_data():
         copyfile(f"{DATASET_PATH}/train/{file}", f"{DATASET_PATH}/anot/{file}")
         os.remove(f"{DATASET_PATH}/train/{file}")
         
+def create_info_file(seed, n_files):
+    date_time = str(datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S"))
+    dataset_name = DATASET_PATH.split("/")[-1]
+
+    info = {
+        "Dataset name: ": dataset_name,
+        "Creation time: ": date_time,
+        "Seed: ": seed,
+        "Total n. of files: ": n_files,
+    }
+
+    with open(os.path.join(DATASET_PATH,'dataset_info.json'), 'w') as file:
+        json.dump(info, file, indent=4)
+    
+    file.close()
 
 merge_data()
-split_data()
+seed, n_files = split_data()
+create_info_file(seed, n_files)
 
-print("Finished splitting data!")
+print(f"Finished splitting data!")
