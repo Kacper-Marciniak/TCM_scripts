e41392258100808e5ecbb5fefba56341c22e28bc
Kacper-Marciniak <kacpermarciniak1998@gmail.com>
2022-06-22 12:43:35 +0200

    Changes to the exception handlers

--- extract_teeth.py
+++ extract_teeth.py
@@ -1,5 +1,4 @@
 import os
-import matplotlib.pyplot as plt
 import numpy as np
 import cv2 as cv
 import prepare_models
@@ -10,9 +9,8 @@ from PARAMETERS import PATH_EXTRACTED_TEETH_DIR
 
 
 # Program variables
-INPUT_IMG_PATH= r"Z:\Dane TCM\skany 2D\20220510_141503"
-#INPUT_IMG_PATH = askdirectory(title="Select folder with images from scanner")     # Path to the folder with the images directly from scaner
-OUTPUT_IMG_PATH  = askdirectory(title="Select DASH-SQL server to write to", initialdir=PATH_EXTRACTED_TEETH_DIR)   # Path to the folder with processed images_F
+INPUT_IMG_PATH = askdirectory(title="Select folder with images from scanner")     # Path to the folder with the images directly from scaner
+OUTPUT_IMG_PATH  = askdirectory(title="Select output directory", initialdir=PATH_EXTRACTED_TEETH_DIR)   # Path to the folder with processed images_F
 DEBUG = False
 
 models = prepare_models.Models()
@@ -29,32 +27,30 @@ for i,image_name in enumerate(files): # Process all available images
     split_name = base_name.split('_')
     row = int(split_name[-1])
     section = int(split_name[-2])
-        
+  
     try:
         im_pth = os.path.join(INPUT_IMG_PATH, image_name)
         im = cv.imread(im_pth) # Read image
         outputs = extraction_predictor(im)
         min_x, min_y, max_x, max_y = list(list(outputs["instances"].to("cpu").pred_boxes)[0].numpy())
-
-    except Exception as e:
-        print(f"There is a problem with the image: {im_pth}")
-        print(f"Exception: {e}")
-        try:
-            source = im_pth
-            destination = OUTPUT_IMG_PATH+"/images/"+image_name
-            shutil.copyfile(source, destination)
-        except Exception as E:
-            print("Can't copy corrupted file")
-            print(f"Exception: {E}")
-
-    try: # Extraction
         roi = im.copy()[int(min_y)-min_y_off:int(max_y)+max_y_off, int(min_x)-min_x_off:int(max_x)+min_x_off] # Extracting ROI
-        print(f"{image_name} extracted - {i+1}/{len_list_files}")
         if DEBUG:
             cv.imshow("ROI", roi)
             cv.waitKey(0)
-        cv.imwrite(f"{OUTPUT_IMG_PATH}/{image_name}" , roi) 
-    except Exception as e:
-        print("Extraction error:", image_name)
-        print(f"Exception: {e}")
+        cv.imwrite(os.path.join(OUTPUT_IMG_PATH,image_name) , roi) 
+        print(f"{image_name} extracted - {i+1}/{len_list_files}")
+
+    except Exception as e1:
+        error_flag = True
+        print(f"There is a problem with the image: {im_pth}")
+        print(f"Exception: {e1}")
+        try:
+            source = im_pth
+            destination = os.path.join(OUTPUT_IMG_PATH,image_name) # if there is problem with the image - simply copy it to the dst dir
+            shutil.copyfile(source, destination)
+            print(f"-->File {source} copied to {destination}")
+        except Exception as e2:
+            print("Can't copy corrupted file")
+            print(f"Exception: {e2}")
+
 print("Finished")
